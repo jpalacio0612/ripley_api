c98d5567b0a1c66458bdf2bd39e5bf369b6c6bd7
jpalacio0612 <jpalacio0612@gmail.com>
2021-10-08 16:13:21 -0500

    update models

--- src/models/recipient.model.js
+++ src/models/recipient.model.js
@@ -4,13 +4,42 @@ const model = mongoose.model;
 
 const recipientSchema = new Schema(
   {
-    name: String,
-    rut: String,
-    email: String,
-    phone: Number,
-    bank: String,
-    accountType: String,
-    accountNumber: Number,
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    rut: {
+      type: String,
+      required: [true, "rut is required"],
+      unique: true,
+      minlength: 9,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Invalid email",
+      ],
+    },
+    phone: {
+      type: String,
+      required: [true, "phone is required"],
+      minlength: 9,
+    },
+    bank: {
+      type: String,
+      required: [true, "bank is required"],
+    },
+    accountType: {
+      type: String,
+      required: [true, "account type is required"],
+    },
+    accountNumber: {
+      type: String,
+      required: [true, "account number is required"],
+    },
     transfers: [{ type: Schema.Types.ObjectId, ref: "Transfer" }],
   },
   {
--- src/models/transfer.model.js
+++ src/models/transfer.model.js
@@ -5,7 +5,11 @@ const model = mongoose.model;
 const transferSchema = new Schema(
   {
     recipient: { type: Schema.Types.ObjectId, ref: "Recipient" },
-    amount: Number,
+    amount: {
+      type: Number,
+      required: [true, "amount is required"],
+      min: 1,
+    },
   },
   {
     timestamps: true,
